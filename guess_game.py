fa22f31868057912a09d78dd0b8303ce7761fdec
chelahcynthia <cchelangat989@gmail.com>
2024-05-16 17:35:17 +0300

    Check if random is a digit

--- guess_game.py
+++ guess_game.py
@@ -1,1 +1,13 @@
 import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+
+
+
+# create a random number and range
+random_number = random.randint(11)
+
