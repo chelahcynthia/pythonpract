f6abab5cffbbf4687427f1e4d5f77e59430d3b31
chelahcynthia <cchelangat989@gmail.com>
2024-11-04 10:39:55 +0300

    Guess  the color code

--- mastermind.py
+++ mastermind.py
@@ -7,3 +7,19 @@ import random
 
 COLORS = ["R", "G", "B", "Y", "W", "O"]
 TRIES = 10
+CODE_LENGTH = 4
+
+# fn to generate the code
+def generate_code():
+    code = []
+    
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+        
+    return code
+
+# guess code
+def guess_code():
+    guess = input("Guess: ").upper().split("")
+    
